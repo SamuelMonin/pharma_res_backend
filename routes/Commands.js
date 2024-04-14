7076f10b38140ec9459c49f4c3b456cfcca06e60
Samuel Monin <sa.monin@eleve.leschartreux.net>
2024-04-14 19:21:47 +0200

    Commands are secure.

--- routes/Commands.js
+++ routes/Commands.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const commandsModel = require('../models/Commands');
+const jwt = require('jsonwebtoken');
+
+const auth = () => (req, res, next) => {
+    if (!req.headers.authorization) {
+      res.status(401).json({ success: false, message: "Token d'authentification non fourni." });
+      return;
+    }
+    try {
+      const token = req.headers.authorization.split(" ")[1];
+      const { login } = jwt.verify(token, 'SAM_S_SECRET');
+      if (!login) {
+        res.status(403).json({ success: false, message: "Votre compte n'a pas la permission pour réaliser cette action." });
+        return;
+      }
+      next();
+    } catch (error) {
+      res.status(401).json({ success: false, message: "Token d'authentification invalide." });
+    }
+  };
 
 router.get('/commands', async (req, res) => {
     try {
@@ -12,8 +31,7 @@ router.get('/commands', async (req, res) => {
     }
 });
 
-router.post('/commands/put-item', async (req, res) => {
-
+router.post('/commands/put-item', auth(), async (req, res) => {
     try {
         await commandsModel.updateOne({
             adress: req.body.adress
